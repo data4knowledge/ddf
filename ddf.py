9887a4e521b42f9b03e50197c3878f8142610699
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-02-22 13:14:22 +0100

    First cut code

---
+++ ddf.py
@@ -0,0 +1,66 @@
+from neo4j import GraphDatabase
+
+the_nodes = {
+    "STUDY": [
+        {
+            "key": "S_1",
+            "study_title": "Umbrella Study of DDR (DNA-Damage Response) Targeting Agents in Advanced Biliary Tract Cancer",
+            "study_version": "???",
+            "study_tag": "???"
+        }
+    ],
+    "STUDY_IDENTIFIER": [
+        {
+            "key": "SI_1",
+            "name": "ClinicalTrials.gov",
+            "id_type": "RegistryID",
+            "org_code": "NCT04298021"
+        },
+        {
+            "key": "SI_2",
+            "name": "ClinicalTrials.gov",
+            "id_type": "RegistryID",
+            "org_code": "NCT04298023"
+        },
+        {
+            "key": "SI_3",
+            "name": "SponsorNo",
+            "id_type": "SponsorID",
+            "org_code": "AZ002020202"
+        }
+    ]
+}
+
+the_relationships = {
+    "HAS_IDENTIFIER": [
+        {"from": "S_1", "to": "SI_1"},
+        {"from": "S_1", "to": "SI_2"},
+        {"from": "S_1", "to": "SI_3"},
+    ]
+}
+
+def clear(tx):
+    tx.run("MATCH (n) DETACH DELETE n")
+
+#def print_friends(tx, name):
+#    for record in tx.run("MATCH (a:Person)-[:KNOWS]->(friend) WHERE a.name = $name "
+#                         "RETURN friend.name ORDER BY friend.name", name=name):
+#        print(record["friend.name"])
+
+driver = GraphDatabase.driver("neo4j://localhost:7687", auth=("neo4j", "ddf"))
+
+with driver.session() as session:
+    session.write_transaction(clear)
+    for key, value in the_nodes.items():
+        query = "UNWIND $nodes as data CREATE (n:%s) SET n = data;" % (key)
+        print(query)
+        result = session.run(query, nodes=value)
+
+    for key, value in the_relationships.items():
+        query = "UNWIND $rels as data MATCH (n {key: data.from}) MATCH (m {key: data.to}) CREATE (n)-[:%s]->(m)" % (key)
+        print(query)
+        result = session.run(query, rels=value)
+
+driver.close()
+
+
---
+++ docs/setup.md
@@ -0,0 +1,6 @@
+```
+pip install neo4j
+python3 -m venv env
+source env/bin/activate
+/Users/daveih/Documents/python/ddf/env/bin/python3 -m pip install --upgrade pip
+```
