a9e34988d46064050876845919bee73c60152c5d
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2022-03-08 12:11:34 +0100

    Refactor, getting blank forms in

--- crf.py
+++ crf.py
@@ -1,56 +1,104 @@
-# Code for generating an SoA from a DDF graph database
-# Code has been quickly put together to get the desired outputs rather than
-# making the code as elegant. It mitem_groupht be possible to merge the queries or 
-# have them return more easily consumed results
+# Code for generating simple Study CRF from the DDF activities list
 
 from neo4j import GraphDatabase
 from beautifultable import BeautifulTable
-
-driver = GraphDatabase.driver("neo4j://localhost:7687", auth=("neo4j", "ddf"))
-
-#with driver.session() as session:
-
-    # Choose a protocol from DB
-#    protocol_name = "DDR"
-
-    # CRF Link
-#    query = """MATCH (pr:STUDY_PROTOCOL)<-[]-(s:STUDY)-[]->(sd:STUDY_DESitem_groupN)-[]->(sc:STUDY_CELL)-[]->(e:STUDY_EPOCH)
-#        -[]->(v:VISIT)<-[]-(wfi:WORKFLOW_ITEM)-[]->(a:ACTIVITY)-[]->(data:STUDY_DATA) WHERE pr.brief_title = '%s'
-#        WITH wfi.description as activity, data.ecrf_link as link
-#        RETURN DISTINCT activity, link""" % (protocol_name)
-#    result = session.run(query)
-#    crf_activities = {}
-#    for record in result:
-#        print("'%s', '%s'" % (record["activity"], record["link"]))
-#        if not record["activity"] in crf_activities:
-#            crf_activities[record["activity"]] = []
-#        crf_activities[record["activity"]].append(record["link"])
-#    print(crf_activities)
-
-#driver.close()
-
-# Display the CRF
-#import urllib.request
-
-#for activity, links in crf_activities.items():
-#    for link in links:
-#        if link != "":
-#            with urllib.request.urlopen(link) as f:
-#                xml = f.read().decode('utf-8')
-#                print(link)
-#                #print(xml)
-
+import urllib.request
 import lxml.etree as ElementTree
-
-
-#from xml.etree import ElementTree
 import datetime
 
 exported_at = datetime.datetime.now().replace(microsecond=0).isoformat()
 odm_namespace = "http://www.cdisc.org/ns/odm/v1.3"
-
 ElementTree.register_namespace('odm', odm_namespace)
 
+def extract_form(xml_doc, study_event_def, form_name, the_forms, the_item_groups, the_items, the_code_lists):
+    forms = xml_doc.findall("//{http://www.cdisc.org/ns/odm/v1.3}FormDef[@Name='%s']" % (form_name))
+    if not forms:
+        return None
+    form = forms[0]
+    the_forms.append(form)
+    form_ref = ElementTree.SubElement(study_event_def, "FormRef")
+    form_ref.set("FormOID", form.attrib["OID"])
+    form_ref.set("Mandatory", "Yes")
+    form_ref.set("OrderNumber", "1")
+
+    item_group_refs = form.findall('{http://www.cdisc.org/ns/odm/v1.3}ItemGroupRef')
+    for item_groupref in item_group_refs:
+        oid = item_groupref.attrib['ItemGroupOID']
+        item_groups = xml_doc.findall("//{http://www.cdisc.org/ns/odm/v1.3}ItemGroupDef[@OID='%s']" % (oid)) 
+        item_group = item_groups[0]
+        the_item_groups.append(item_group)
+        item_refs = item_group.findall('{http://www.cdisc.org/ns/odm/v1.3}ItemRef')
+        for item_ref in item_refs:
+            oid = item_ref.attrib['ItemOID']
+            items = xml_doc.findall("//{http://www.cdisc.org/ns/odm/v1.3}ItemDef[@OID='%s']" % (oid)) 
+            item = items[0]
+            the_items.append(item)
+            code_list_refs = item.findall('{http://www.cdisc.org/ns/odm/v1.3}CodeListRef')
+            for code_list_ref in code_list_refs:
+                oid = code_list_ref.attrib['CodeListOID']
+                code_lists = xml_doc.findall("//{http://www.cdisc.org/ns/odm/v1.3}CodeList[@OID='%s']" % (oid)) 
+                code_list = code_lists[0]
+                the_code_lists.append(code_list)
+            measurement_unit_refs = item.findall('{http://www.cdisc.org/ns/odm/v1.3}MeasurementUnitRef')
+            for measurement_unit_ref in measurement_unit_refs:
+                oid = measurement_unit_ref.attrib['MeasurementUnitOID']
+                measurement_units = xml_doc.findall("//{http://www.cdisc.org/ns/odm/v1.3}MeasurementUnit[@OID='%s']" % (oid)) 
+                if measurement_units:
+                    measurement_unit = measurement_units[0]
+                    basic_definitions.append(measurement_unit)
+
+def blank_form(study_event_def, form_name, the_forms, the_item_groups, the_items, the_code_lists):
+    form = ElementTree.Element("{%s}FormDef" % (odm_namespace))
+    form.set("OID", "DDF_F_%s" % (len(the_forms) + 1)) 
+    form.set("Name", form_name) 
+    form.set("Repeating", "No") 
+    the_forms.append(form)
+    form_ref = ElementTree.SubElement(study_event_def, "FormRef")
+    form_ref.set("FormOID", form.attrib["OID"])
+    form_ref.set("Mandatory", "Yes")
+    form_ref.set("OrderNumber", "1")
+    item_group_ref = ElementTree.SubElement(form, "{%s}ItemGroupRef" % (odm_namespace))
+    item_group_ref.set("ItemGroupOID", "DDF_F_%s_IG" % (len(the_forms) + 1)) 
+    item_group_ref.set("OrderNumber", "1") 
+    item_group = ElementTree.Element("{%s}ItemGroupDef" % (odm_namespace))
+    item_group.set("OID", "DDF_F_%s_IG" % (len(the_forms) + 1)) 
+    item_group.set("Name", form_name) 
+    item_group.set("Repeating", "No") 
+    item_ref = ElementTree.SubElement(form, "{%s}ItemRef" % (odm_namespace))
+    item_ref.set("ItemOID", "DDF_F_%s_IG_I" % (len(the_forms) + 1)) 
+    item_ref.set("Mandatory", "Yes")
+    item_ref.set("OrderNumber", "1")
+    item = ElementTree.Element("{%s}ItemDef" % (odm_namespace))
+    item.set("OID", "DDF_F_%s_IG_I" % (len(the_forms) + 1)) 
+    item.set("Name", form_name) 
+    item.set("Datatype", "Text") 
+    question = ElementTree.SubElement(item, "{%s}Question" % (odm_namespace))
+    translated_text = ElementTree.SubElement(question, "{%s}Question" % (odm_namespace))
+    translated_text.text = "Placeholder for activity %s" % (form_name)
+
+driver = GraphDatabase.driver("neo4j://localhost:7687", auth=("neo4j", "ddf"))
+with driver.session() as session:
+
+    # Choose a protocol from DB
+    protocol_name = "DDR"
+
+    # CRF Link
+    query = """MATCH (pr:STUDY_PROTOCOL)<-[]-(s:STUDY)-[]->(sd:STUDY_DESIGN)-[]->(sc:STUDY_CELL)-[]->(e:STUDY_EPOCH)
+        -[]->(v:VISIT)<-[]-(wfi:WORKFLOW_ITEM)-[]->(a:ACTIVITY)-[]->(data:STUDY_DATA) WHERE pr.brief_title = '%s'
+        WITH a.description as activity, data.ecrf_link as link
+        RETURN DISTINCT activity, link""" % (protocol_name)
+    result = session.run(query)
+    crf_activities = {}
+    for record in result:
+        #print("'%s', '%s'" % (record["activity"], record["link"]))
+        if not record["activity"] in crf_activities:
+            crf_activities[record["activity"]] = []
+        crf_activities[record["activity"]].append(record["link"])
+    #print(crf_activities)
+
+driver.close()
+
+# Display the CRF
 odm = ElementTree.Element("{%s}ODM" % (odm_namespace))
 odm.set("FileOID", "DDF_ODM_001") 
 odm.set("FileType", "Snapshot") 
@@ -79,49 +127,34 @@ study_event_def.set("OID", "DDF_SE_001")
 study_event_def.set("Name", "CRF Book")
 study_event_def.set("Repeating", "No")
 study_event_def.set("Type", "Scheduled")
-#				<FormRef FormOID="0001_F_BASELINE" Mandatory="Yes" OrderNumber="1"/>
 
 the_forms = []
 the_item_groups = []
 the_items = []
 the_code_lists = []
 
-mytree = ElementTree.parse('odm.xml')
-myroot = mytree.getroot()
-forms = mytree.findall('//{http://www.cdisc.org/ns/odm/v1.3}FormDef[@Name="Baseline Visit Form"]')
-form = forms[0]
-the_forms.append(form)
-form_ref = ElementTree.SubElement(study_event_def, "FormRef")
-form_ref.set("FormOID", form.attrib["OID"])
-form_ref.set("Mandatory", "Yes")
-form_ref.set("OrderNumber", "1")
+# Go and find the forms. Either use the CRF link or, if not present, use a standard file of forms, see if we have a match
+for activity, links in crf_activities.items():
+    print(activity)
+    for link in links:
+        result = None
+        if link == "":
+            print("    No CRF link")
+            xml_doc = ElementTree.parse('odm.xml')
+            result = extract_form(xml_doc, study_event_def, activity, the_forms, the_item_groups, the_items, the_code_lists)
+        else:
+            print("    CRF link detected")
+            with urllib.request.urlopen(link) as f:
+                xml_doc = ElementTree.parse('odm.xml')
+                result = extract_form(xml_doc, study_event_def, activity, the_forms, the_item_groups, the_items, the_code_lists)
+        if result == None:
+            print("    Blank form needed")
+            xml_doc = ElementTree.parse('blank.xml')
+            result = blank_form(study_event_def, activity, the_forms, the_item_groups, the_items, the_code_lists)
+        else:
+            print("    Form added")
 
-item_group_refs = form.findall('{http://www.cdisc.org/ns/odm/v1.3}ItemGroupRef')
-for item_groupref in item_group_refs:
-    oid = item_groupref.attrib['ItemGroupOID']
-    item_groups = mytree.findall("//{http://www.cdisc.org/ns/odm/v1.3}ItemGroupDef[@OID='%s']" % (oid)) 
-    item_group = item_groups[0]
-    the_item_groups.append(item_group)
-    item_refs = item_group.findall('{http://www.cdisc.org/ns/odm/v1.3}ItemRef')
-    for item_ref in item_refs:
-        oid = item_ref.attrib['ItemOID']
-        items = mytree.findall("//{http://www.cdisc.org/ns/odm/v1.3}ItemDef[@OID='%s']" % (oid)) 
-        item = items[0]
-        the_items.append(item)
-        code_list_refs = item.findall('{http://www.cdisc.org/ns/odm/v1.3}CodeListRef')
-        for code_list_ref in code_list_refs:
-            oid = code_list_ref.attrib['CodeListOID']
-            code_lists = mytree.findall("//{http://www.cdisc.org/ns/odm/v1.3}CodeList[@OID='%s']" % (oid)) 
-            code_list = code_lists[0]
-            the_code_lists.append(code_list)
-        measurement_unit_refs = item.findall('{http://www.cdisc.org/ns/odm/v1.3}MeasurementUnitRef')
-        for measurement_unit_ref in measurement_unit_refs:
-            oid = measurement_unit_ref.attrib['MeasurementUnitOID']
-            measurement_units = mytree.findall("//{http://www.cdisc.org/ns/odm/v1.3}MeasurementUnit[@OID='%s']" % (oid)) 
-            if measurement_units:
-                measurement_unit = measurement_units[0]
-                basic_definitions.append(measurement_unit)
-
+# Add the items into the ODM
 for form in the_forms:
     metadata_version.append(form)
 for item_group in the_item_groups:
@@ -131,13 +164,13 @@ for item in the_items:
 for code_list in the_code_lists:
     metadata_version.append(code_list)
 
+
+# Write out the XML merged file
 tree = ElementTree.ElementTree(odm)
 tree.write("ddf_crf.xml", xml_declaration=True, encoding='utf-8', method="xml")
 
+# Transform the XML into an HTML rendering
 xslt = ElementTree.parse("crf.xsl")
 transform = ElementTree.XSLT(xslt)
 newdom = transform(odm)
 newdom.write("ddf_crf.html", xml_declaration=True, encoding='utf-8', method="html")
-#f = open("ddf_crf.html", "a")
-#f.write(newdom)
-#.close()
